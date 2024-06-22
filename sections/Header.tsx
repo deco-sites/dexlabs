a91b2fb80712979fcad1d7e97a97ff77405edd7f
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-06-22 05:23:10 -0300

    style: mobile menu styled

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,6 +1,7 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Icon from "../components/ui/Icon.tsx";
+import { useScroll } from "site/sdk/useScroll.ts";
 
 export interface CTA {
   id?: string;
@@ -43,26 +44,26 @@ export default function Header({
   },
 }: Nav) {
   return (
-    <nav class="drawer drawer-end">
+    <>
       <div class="h-28"></div>
-      <div class="fixed w-full bg-secondary z-50">
+      <nav class="drawer drawer-end fixed top-0 w-full bg-secondary z-50">
         <input id="mobile-drawer-nav" type="checkbox" class="drawer-toggle" />
 
         {/* main content */}
         <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-8 max-w-[1378px]">
 
           <div class="hidden items-center justify-between lg:flex w-full">
-            <div class="flex gap-[70px] items-center bg-neutral border border-primary rounded-full py-2 pl-4 pr-8">
+            <div class="flex gap-10 items-center bg-neutral border border-primary rounded-full py-2 pl-4 pr-8">
               <a href="/">
                 <Image src={logo.src || ""} width={67} height={36} alt={logo.alt} />
               </a>
-              <ul class="flex gap-[70px]">
+              <ul class="flex gap-10">
                 {navigation.links.map((link) => (
                   <li>
                     <a
                       href={link.url}
                       aria-label={link.label}
-                      class="link no-underline hover:underline text-primary"
+                      class="link no-underline hover:bg-primary hover:text-secondary text-primary py-2.5 px-3.5 rounded-full transition duration-200"
                     >
                       {link.label}
                     </a>
@@ -77,7 +78,7 @@ export default function Header({
                   id={item?.id}
                   href={item?.href ?? "#"}
                   target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`font-normal btn btn-primary text-secondary font-medium rounded-full min-h-9 h-9 ${item.outline && "btn-outline"
+                  class={`font-normal btn btn-primary text-secondary font-medium rounded-full min-h-9 h-9 text-lg ${item.outline && "btn-outline"
                     }`}
                 >
                   {item?.text}
@@ -92,54 +93,58 @@ export default function Header({
           >
             <Icon id="Bars3" size={24} strokeWidth={0.1} />
           </label>
+
         </div>
-      </div>
 
-      {/* sidebar */}
-      <aside class="drawer-side z-50">
-        {/* Close when clicking on overlay */}
-        <label
-          htmlFor="mobile-drawer-nav"
-          aria-label="close sidebar"
-          class="drawer-overlay"
-        />
+        {/* sidebar */}
+        <aside class="drawer-side z-50">
+          {/* Close when clicking on overlay */}
+          <label
+            htmlFor="mobile-drawer-nav"
+            aria-label="close sidebar"
+            class="drawer-overlay"
+          />
 
-        <div class="flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content">
-          <a class="p-4" href="/">
-            <Image
-              src={logo.src || ""}
-              width={100}
-              height={28}
-              alt={logo.alt}
-            />
-          </a>
+          <div class="flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content p-4 bg-opacity-80 backdrop-blur-sm">
+            <div class="bg-neutral border border-primary rounded-xl">
 
-          <ul class="menu">
-            {navigation?.links.map((link) => (
-              <li>
-                <a href={link.url} aria-label={link.label} class="text-primary">
-                  {link.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-
-          <ul class="p-4 flex items-center gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item?.id}
-                id={item?.id}
-                href={item?.href ?? "#"}
-                target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary rounded-full min-h-9 h-9 ${item.outline && "btn-outline"
-                  }`}
-              >
-                {item?.text}
+              <a class="p-4" href="/">
+                <Image
+                  src={logo.src || ""}
+                  width={100}
+                  height={28}
+                  alt={logo.alt}
+                />
               </a>
-            ))}
-          </ul>
-        </div>
-      </aside>
-    </nav>
+
+              <ul class="menu">
+                {navigation?.links.map((link) => (
+                  <li>
+                    <a href={link.url} aria-label={link.label} class="no-underline hover:bg-primary hover:text-secondary text-primary">
+                      {link.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+
+              <ul class="p-4 flex items-center gap-3">
+                {navigation.buttons?.map((item) => (
+                  <a
+                    key={item?.id}
+                    id={item?.id}
+                    href={item?.href ?? "#"}
+                    target={item?.href.includes("http") ? "_blank" : "_self"}
+                    class={`font-normal btn btn-primary rounded-full min-h-9 h-9 ${item.outline && "btn-outline"
+                      }`}
+                  >
+                    {item?.text}
+                  </a>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </aside>
+      </nav>
+    </>
   );
 }
