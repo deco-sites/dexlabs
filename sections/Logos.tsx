7e1747e915a368d22969bdc69251da2b8568cd23
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-06-23 00:11:07 -0300

    style: logos carousel height ajusted

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -30,7 +30,7 @@ export default function Logos({
             src={logo.src || ""}
             alt={logo.altText || ""}
             width={110}
-            height={25}
+            height={110}
           />
         );
       })}
@@ -40,8 +40,8 @@ export default function Logos({
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 py-6 lg:py-14">
       <div class="flex flex-col gap-12">
         <p class="text-center text-lg">{title}</p>
-        <div class="relative w-full overflow-hidden h-11">
-          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
+        <div class="relative w-full overflow-hidden h-28">
+          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-full">
             {slideContent}
           </div>
         </div>
