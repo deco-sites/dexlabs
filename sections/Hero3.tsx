f10cf5483a8254ee400ef8be6bf9f3a0451b60d4
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-06-23 05:00:22 -0300

    style: hero3 max width setted, hero3 items container flex changed to flex-wrap

--- sections/Hero3.tsx
+++ sections/Hero3.tsx
@@ -19,11 +19,11 @@ export interface Props {
 }
 
 export default function Hero3({ title, titleCaption, caption, items = [] }: Props) {
-    return <section>
+    return <section class="max-w-[1378px] mx-auto">
         <h3 class="text-center text-xl text-accent font-light">{titleCaption || ""}</h3>
         <h1 class="py-5 font-semibold text-3xl text-[60px] leading-[64px] text-center text-primary">{title}</h1>
         <h2 class="max-w-[630px] mx-auto text-primary text-2xl font-normal leading-7 text-center">{caption || ""}</h2>
-        <div class="flex justify-center mt-12 gap-12">
+        <div class="flex flex-wrap justify-center mt-12 gap-12">
             {items.length && items.map((item) => (<div class="w-[365px] flex flex-col items-left text-primary">
                 <div class="h-[80px] flex item-bottom">
                     <Image
