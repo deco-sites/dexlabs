04f691810a623c73d70e8ef1e82db0a59ef44251
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-06-24 14:25:38 -0300

    style: style tweaks in the ImageWithParagraph section to better match the figma design

--- sections/ImageWithParagraph.tsx
+++ sections/ImageWithParagraph.tsx
@@ -75,13 +75,13 @@ export default function ImageWithParagraph({
           />
         </div>
         <div class={`w-full md:w-1/2 space-y-2 md:space-y-4 md:max-w-xl gap-4 z-10 ${textJustification[justifyText]}`}>
-          <p class="text-sm font-semibold">
+          <p class="text-xl font-light">
             {tagline}
           </p>
-          <p class="text-4xl leading-snug">
+          <p class="text-6xl font-semibold">
             {title}
           </p>
-          <p class={`leading-normal`}>
+          <p class={'leading-normal font-normal text-2xl'}>
             {description}
           </p>
           <div class={`flex gap-3 pt-4 ${flexDirection[justifyText]}`}>
@@ -91,24 +91,15 @@ export default function ImageWithParagraph({
                 id={item?.id}
                 href={item?.href}
                 target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary
+                class={`font-normal btn btn-primary text-xl font-bold transition-all duration-200
                   ${!item.style || item.style == "Outline" && "btn-outline"}
-                  ${item.style == "Ghost" && "btn-ghost"}
+                  ${item.style == "Ghost" && "btn-ghost hover:bg-transparent underline hover:text-info font-extrabold"}
                 `}
               >
                 {item?.text}
                 {item.style == "Ghost" && (
-                  <svg
-                    width="24"
-                    height="25"
-                    viewBox="0 0 24 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M9.70697 16.9767L15.414 11.2697L9.70697 5.56274L8.29297 6.97674L12.586 11.2697L8.29297 15.5627L9.70697 16.9767Z"
-                      fill="#18181B"
-                    />
+                  <svg class="fill-current" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16.0306 11.401L11.5306 15.901C11.3897 16.0419 11.1986 16.121 10.9994 16.121C10.8001 16.121 10.609 16.0419 10.4681 15.901C10.3272 15.7601 10.2481 15.569 10.2481 15.3697C10.2481 15.1705 10.3272 14.9794 10.4681 14.8385L13.6875 11.6203H4.5C4.30109 11.6203 4.11032 11.5413 3.96967 11.4007C3.82902 11.26 3.75 11.0693 3.75 10.8703C3.75 10.6714 3.82902 10.4807 3.96967 10.34C4.11032 10.1994 4.30109 10.1203 4.5 10.1203H13.6875L10.4694 6.90035C10.3285 6.75945 10.2493 6.56836 10.2493 6.3691C10.2493 6.16984 10.3285 5.97874 10.4694 5.83785C10.6103 5.69695 10.8014 5.6178 11.0006 5.6178C11.1999 5.6178 11.391 5.69695 11.5319 5.83785L16.0319 10.3378C16.1018 10.4076 16.1573 10.4905 16.1951 10.5818C16.2329 10.6731 16.2523 10.7709 16.2522 10.8697C16.252 10.9685 16.2324 11.0662 16.1944 11.1574C16.1564 11.2486 16.1007 11.3314 16.0306 11.401Z" />
                   </svg>
                 )}
               </a>
