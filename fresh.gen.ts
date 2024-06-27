a0533c66f1e121a9592bac7216566651a920462d
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-06-27 16:49:03 -0300

    style: scroll animations added to the hero as it is in the reference model

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_app from "./routes/_app.tsx";
 import * as $Header from "./islands/Header.tsx";
+import * as $Hero from "./islands/Hero.tsx";
 import * as $ImageSelector from "./islands/ImageSelector.tsx";
 import * as $Video from "./islands/Video.tsx";
 import { type Manifest } from "$fresh/server.ts";
@@ -14,6 +15,7 @@ const manifest = {
   },
   islands: {
     "./islands/Header.tsx": $Header,
+    "./islands/Hero.tsx": $Hero,
     "./islands/ImageSelector.tsx": $ImageSelector,
     "./islands/Video.tsx": $Video,
   },
