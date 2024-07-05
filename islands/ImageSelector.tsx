c04e1b98550c82090b2250ae52275944c49bbb07
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-05 17:40:14 -0300

    style: product page image max width set to 870px, product page image background set to 1230x590

--- islands/ImageSelector.tsx
+++ islands/ImageSelector.tsx
@@ -27,13 +27,13 @@ export default function ImageSelector({ backgroundImage, images = [] }: Props) {
 
     return <section class="overflow-hidden">
 
-        <div class={`${transitionClass} ${scrolled ? 'max-w-[1440px]' : 'max-w-full mt-24'} mx-auto relative min-h-[816px] p-8`}>
-            {backgroundImage && <div class="absolute w-full -z-10 left-0 top-0"><Image
+        <div class={`${transitionClass} ${scrolled ? 'max-w-[1230px] h-[590px]' : 'max-w-full mt-24 h-[816px]'} mx-auto relative p-8`}>
+            {backgroundImage && <div class="absolute w-full h-full -z-10 left-0 top-0"><Image
                 src={backgroundImage}
                 alt="Background Image"
                 width={1440}
                 height={816}
-                class={`w-full object-cover ${transitionClass} ${scrolled && 'rounded-xl lg:rounded-[42px]'}`}
+                class={`w-full h-full object-cover ${transitionClass} ${scrolled && 'rounded-xl lg:rounded-[42px]'}`}
             /></div>}
             <div class={`flex justify-end max-w-[1440px] mx-auto ${transitionClass} ${!scrolled && 'opacity-0'}`}>
                 <div class="min-h-14 flex flex-wrap justify-center lg:justify-end gap-4 border border-primary rounded-lg lg:rounded-full bg-neutral mb-6">
@@ -48,13 +48,13 @@ export default function ImageSelector({ backgroundImage, images = [] }: Props) {
                 </div>
             </div>
 
-            <div class={`relative w-full max-h-[600px] flex justify-center z-10`}>
+            <div class={`relative w-full max-h-[600px] flex justify-center z-10 mx-auto ${transitionClass} ${scrolled ? 'max-w-[870px] h-[490px]' : 'max-w-[1440px]'}`}>
                 <div class={`absolute ${transitionClass} ${scrolled ? 'top-0' : 'top-[-200px]'}`}>
                     <Image
                         src={images[selectedImage].image}
                         alt={images[selectedImage].alt || ""}
                         width={1280}
-                        class={`object-cover md:object-contain`}
+                        class={`h-full object-cover md:object-contain`}
                     />
                 </div>
             </div>
