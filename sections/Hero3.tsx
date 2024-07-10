8c68ec0c418c573c2454ce0753f51658eabbc86d
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-10 07:18:10 -0300

    style: hero3 items title font size changed to 24px and images set to max 60px max height

--- sections/Hero3.tsx
+++ sections/Hero3.tsx
@@ -25,14 +25,15 @@ export default function Hero3({ title, titleCaption, caption, items = [] }: Prop
         <h2 class="max-w-[630px] mx-auto text-primary text-xl md:text-2xl font-normal md:leading-7 text-center">{caption || ""}</h2>
         <div class="flex flex-wrap justify-center mt-12 gap-12">
             {items.length && items.map((item) => (<div class="w-2/5 md:w-[365px] flex flex-col items-center md:items-start text-primary">
-                <div class="h-[80px] flex item-bottom">
+                <div class="h-[60px] flex item-bottom">
                     <Image
                         width={80}
                         alt={item.imageAlt || ""}
                         src={item.image}
+                        class="h-full"
                     />
                 </div>
-                <h2 class="font-semibold text-xl md:text-3xl py-4">{item.title}</h2>
+                <h2 class="font-semibold text-xl md:text-2xl py-4">{item.title}</h2>
                 <p class="text-base md:text-lg text-center md:text-left">{item.description}</p>
             </div>))}
         </div>
