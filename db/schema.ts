c7e36470463116b27863145029a0b11484fd6255
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-15 18:21:44 -0300

    deco records setup

---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const profiles = sqliteTable("profiles", {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    name: text("name"),
+    email: text("email"),
+});
