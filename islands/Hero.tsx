03a3a893e2174f2bab4ec86423ecc33a392d3cc9
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-17 19:19:53 -0300

    style: now the Hero buttons have the same animation as the retool website

--- islands/Hero.tsx
+++ islands/Hero.tsx
@@ -94,10 +94,13 @@ export default function HeroFlats({
                                     id={item?.id}
                                     href={item?.href}
                                     target={item?.href.includes("http") ? "_blank" : "_self"}
-                                    class={`font-normal btn btn-primary font-medium rounded-full min-h-10 h-10 text-lg hover:bg-secondary text-secondary hover:text-primary ${item.outline && "btn-outline"
+                                    class={`overflow-hidden font-normal btn btn-primary px-0 font-medium rounded-full min-h-10 h-10 text-lg hover:bg-secondary ${item.outline && "btn-outline"
                                         }`}
                                 >
-                                    {item?.text}
+                                    <div class={`flex flex-col px-4 relative hover:-translate-y-full transition-transform duration-500 ease-in-out`}>
+                                        <span class={`${item.outline ? 'text-primary' : 'text-secondary'}`}>{item?.text}</span>
+                                        <span class={`absolute top-full ${item.outline ? 'text-secondary' : 'text-primary'}`}>{item?.text}</span>
+                                    </div>
                                 </a>
                             ))}
                         </div>
