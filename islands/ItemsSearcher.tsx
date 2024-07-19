7ae0da530d5ed8330748f9013b583a686e174a75
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-18 22:20:00 -0300

    feat: now the integration cards load as you scroll and click in show more button

--- islands/ItemsSearcher.tsx
+++ islands/ItemsSearcher.tsx
@@ -22,14 +22,17 @@ export interface Item {
 
 export interface Props {
     requestConnector: IRequestConnector;
+    showMoreText?: string;
+    itemsPerPage?: number;
     categories: string[];
     items: Item[];
 }
 
-export default function ({ categories, items, requestConnector }: Props) {
+export default function ({ categories, items, requestConnector, showMoreText, itemsPerPage = 50 }: Props) {
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedOrder, setSelectedOrder] = useState('Asc');
     const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    const [limit, setLimit] = useState(itemsPerPage);
 
     let filteredItems = items;
 
@@ -39,6 +42,9 @@ export default function ({ categories, items, requestConnector }: Props) {
 
     if (selectedOrder == 'Asc') filteredItems = filteredItems.sort((a, b) => a.title.localeCompare(b.title));
     else filteredItems = filteredItems.sort((a, b) => b.title.localeCompare(a.title));
+
+    const originalZise = filteredItems.length;
+    filteredItems = filteredItems.slice(0, limit);
 
     const handleOrderChange = (event: any) => {
         setSelectedOrder(event.target.value || 'Asc');
@@ -94,6 +100,14 @@ export default function ({ categories, items, requestConnector }: Props) {
                 {filteredItems.length == 0
                     ? <RequestConnector {...requestConnector} />
                     : filteredItems.map((item) => <IntegrationCard title={item.title} image={item.image || ""} />)}
+            </div>
+            <div class="flex justify-center mb-24 mt-8">
+                {limit < originalZise && <button onClick={() => setLimit(limit + itemsPerPage)} class="underline text-primary hover:text-info font-extrabold text-xl flex items-center transition-all duration-200">
+                    {showMoreText || "Show more"}
+                    <svg class="fill-current" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.0306 11.401L11.5306 15.901C11.3897 16.0419 11.1986 16.121 10.9994 16.121C10.8001 16.121 10.609 16.0419 10.4681 15.901C10.3272 15.7601 10.2481 15.569 10.2481 15.3697C10.2481 15.1705 10.3272 14.9794 10.4681 14.8385L13.6875 11.6203H4.5C4.30109 11.6203 4.11032 11.5413 3.96967 11.4007C3.82902 11.26 3.75 11.0693 3.75 10.8703C3.75 10.6714 3.82902 10.4807 3.96967 10.34C4.11032 10.1994 4.30109 10.1203 4.5 10.1203H13.6875L10.4694 6.90035C10.3285 6.75945 10.2493 6.56836 10.2493 6.3691C10.2493 6.16984 10.3285 5.97874 10.4694 5.83785C10.6103 5.69695 10.8014 5.6178 11.0006 5.6178C11.1999 5.6178 11.391 5.69695 11.5319 5.83785L16.0319 10.3378C16.1018 10.4076 16.1573 10.4905 16.1951 10.5818C16.2329 10.6731 16.2523 10.7709 16.2522 10.8697C16.252 10.9685 16.2324 11.0662 16.1944 11.1574C16.1564 11.2486 16.1007 11.3314 16.0306 11.401Z" />
+                    </svg>
+                </button>}
             </div>
         </div>
     </section>
