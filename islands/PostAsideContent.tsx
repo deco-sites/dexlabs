179c69531f5b8aed2d27ea1027c5c28b577646d1
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-21 19:54:44 -0300

    style: blogPost floating button aligned with text when there is no aside info

--- islands/PostAsideContent.tsx
+++ islands/PostAsideContent.tsx
@@ -10,7 +10,7 @@ export interface Props {
 export default function PostAsideContent({ asideLogo, asideInfo, floatingButton = { text: "Book a demo", href: "/bookademo", title: "Ready to try deX Lake Platform?" } }: Props) {
     return (
         <aside class="w-full lg:w-[275px] flex flex-col mt-24 pb-16 lg:mt-0">
-            {(asideInfo || asideLogo) && <div class="bg-info-content rounded-2xl shadow-custom-box flex flex-col px-9 py-12 gap-9">
+            {(asideInfo || asideLogo) && <div class="bg-info-content rounded-2xl shadow-custom-box flex flex-col mb-14 px-9 py-12 gap-9">
                 {asideLogo?.src && <div class="w-full">
                     <Image
                         width={300}
@@ -25,7 +25,7 @@ export default function PostAsideContent({ asideLogo, asideInfo, floatingButton 
                     </div>
                 ))}
             </div>}
-            <div class="h-full lg:h-full w-full -top-52 lg:top-0 left-0 mt-14 absolute lg:relative">
+            <div class="h-full lg:h-full w-full -top-52 lg:top-0 left-0 absolute lg:relative">
                 <div class="bg-primary text-secondary px-5 py-4 lg:py-7 gap-6 flex flex-col items-center rounded-2xl sticky top-28 lg:top-[40vh]">
                     <h2 class="text-center font-semibold text-xl lg:text-2xl">{floatingButton.title}</h2>
                     <a
