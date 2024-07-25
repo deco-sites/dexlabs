33cbee46c7cbf359b5cfd877b0624e2c59446b4c
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-25 15:33:45 -0300

    style: DetailedHero spacing modified

--- sections/DetailedHero.tsx
+++ sections/DetailedHero.tsx
@@ -49,66 +49,64 @@ export default function ({ title, titleCaption, caption, cta, heroSections, id }
         4: 'w-1/4'
     };
     return <section class="bg-primary" id={id || ""}>
-        <div class="max-w-[1378px] md:rounded-[40px] mx-auto p-4 md:p-14 text-center lg:text-left">
-            <GlobalDesign>
+        <div class="max-w-[1140px] md:rounded-[40px] mx-auto py-4 md:py-12 text-center lg:text-left">
 
-                <h4 class="text-lg md:text-xl text-secondary font-light">{titleCaption || ""}</h4>
-                <h2 class="py-5 font-medium text-3xl md:text-[60px] md:leading-[64px] text-secondary">{title}</h2>
-                <h3 class="mx-auto text-secondary text-xl md:text-2xl font-normal">{caption || ""}</h3>
-                <div class="flex items-center gap-3 flex-wrap justify-center lg:justify-start py-10">
-                    {cta?.map((item) => (
-                        <a
-                            key={item?.id}
-                            id={item?.id}
-                            href={item?.href}
-                            target={item?.href.includes("http") ? "_blank" : "_self"}
-                            class={`overflow-hidden font-normal btn px-0 font-semibold rounded-full min-h-10 h-10 text-lg text-primary border-none transition-all duration-200
+            <h4 class="text-lg md:text-xl text-secondary font-light">{titleCaption || ""}</h4>
+            <h2 class="pt-10 pb-6 font-medium text-3xl md:text-[60px] md:leading-[64px] text-secondary">{title}</h2>
+            <h3 class="mx-auto text-secondary text-xl md:text-2xl font-normal">{caption || ""}</h3>
+            <div class="flex items-center gap-3 flex-wrap justify-center lg:justify-start pt-10 pb-8">
+                {cta?.map((item) => (
+                    <a
+                        key={item?.id}
+                        id={item?.id}
+                        href={item?.href}
+                        target={item?.href.includes("http") ? "_blank" : "_self"}
+                        class={`overflow-hidden font-normal btn px-0 font-semibold rounded-full min-h-10 h-10 text-lg text-primary border-none transition-all duration-200
                                 ${item.outline ? 'bg-success hover:bg-secondary' : 'bg-secondary hover:bg-success'}`
-                            }
-                        >
-                            <div class={`flex flex-col px-4 relative hover:-translate-y-full transition-transform duration-500 ease-in-out`}>
-                                <span class={` ${item.outline ? 'text-primary' : 'text-primary'}`}>
-                                    {item?.text}
+                        }
+                    >
+                        <div class={`flex flex-col px-4 relative hover:-translate-y-full transition-transform duration-500 ease-in-out`}>
+                            <span class={` ${item.outline ? 'text-primary' : 'text-primary'}`}>
+                                {item?.text}
 
 
-                                </span>
-                                <span class={`absolute top-full ${item.outline ? 'text-primary' : 'text-primary'}`}>
-                                    {item?.text}
+                            </span>
+                            <span class={`absolute top-full ${item.outline ? 'text-primary' : 'text-primary'}`}>
+                                {item?.text}
 
-                                </span>
-                            </div>
-                        </a>
-                    ))}
-                </div>
-                <div class="flex flex-wrap w-full justify-center 2xl:justify-between mt-8 gap-4 xl:gap-0">
-                    {heroSections?.map((section) => (
-                        <div class={`p-6 rounded-xl max-w-[356px] flex flex-col gap-6 ${section.border && 'border border-warning'}`}>
-                            <div class="flex gap-4">
-                                {section.TitleIcon?.image && <Image
-                                    width={33}
-                                    src={section.TitleIcon.image}
-                                    alt={section.TitleIcon.alt || ""}
-                                />}
-                                <h2 class="text-secondary font-semibold text-3xl">{section.Title}</h2>
-                            </div>
-                            <p class="text-secondary font-normal text-xl">{section.Text}</p>
-                            <div class="flex flex-wrap justify-center">
-                                {section.Images?.map((img) => (
-                                    <div class={`${imageWidth[section.ImagesPerLine || 1]} p-1`}>
-                                        <Image
-                                            width={360}
-                                            src={img.image || ""}
-                                            alt={img.alt || ""}
-                                        />
-                                    </div>
-                                )
-                                )}
-                            </div>
+                            </span>
                         </div>
-                    )
-                    )}
-                </div>
-            </GlobalDesign>
+                    </a>
+                ))}
+            </div>
+            <div class="flex flex-wrap w-full justify-center 2xl:justify-between gap-4 xl:gap-0">
+                {heroSections?.map((section) => (
+                    <div class={`p-6 rounded-xl max-w-[356px] flex flex-col gap-6 ${section.border && 'border border-warning'}`}>
+                        <div class="flex gap-4">
+                            {section.TitleIcon?.image && <Image
+                                width={33}
+                                src={section.TitleIcon.image}
+                                alt={section.TitleIcon.alt || ""}
+                            />}
+                            <h2 class="text-secondary font-semibold text-3xl">{section.Title}</h2>
+                        </div>
+                        <p class="text-secondary font-normal text-xl">{section.Text}</p>
+                        <div class="flex flex-wrap justify-center">
+                            {section.Images?.map((img) => (
+                                <div class={`${imageWidth[section.ImagesPerLine || 1]} p-1`}>
+                                    <Image
+                                        width={360}
+                                        src={img.image || ""}
+                                        alt={img.alt || ""}
+                                    />
+                                </div>
+                            )
+                            )}
+                        </div>
+                    </div>
+                )
+                )}
+            </div>
         </div>
     </section>
 }
