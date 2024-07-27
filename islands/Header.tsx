68ccedfe89e6da72ac65cb0281e2e99c80c7a914
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-26 22:48:42 -0300

    style: header buttons background hover color change removed

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -150,12 +150,12 @@ export default function Header({
                                         id={item?.id}
                                         href={item?.href ?? "#"}
                                         target={item?.href.includes("http") ? "_blank" : "_self"}
-                                        class={`overflow-hidden font-normal btn btn-primary px-0 text-secondary font-medium rounded-full min-h-10 h-10 text-lg hover:bg-secondary ${item.outline && "btn-outline"
+                                        class={`overflow-hidden font-normal btn btn-primary px-0 text-secondary font-medium rounded-full min-h-10 h-10 text-lg  ${item.outline && "bg-secondary hover:bg-secondary !text-primary"
                                             }`}
                                     >
                                         <div class={`flex flex-col px-4 relative hover:-translate-y-full transition-transform duration-500 ease-in-out`}>
                                             <span class="">{item?.text}</span>
-                                            <span class={`absolute top-full ${item.outline ? 'text-secondary' : 'text-primary'}`}>{item?.text}</span>
+                                            <span class={`absolute top-full ${item.outline ? 'text-primary' : 'text-secondary'}`}>{item?.text}</span>
                                         </div>
                                     </a>
                                 ))}
